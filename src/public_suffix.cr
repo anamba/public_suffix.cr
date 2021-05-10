f5b826d19a148c047db224f0d4cd68f34333d6e6
Aaron Namba <aaron@aaronnamba.com>
2021-05-10 13:07:49 +0900

    updated list

--- src/public_suffix.cr
+++ src/public_suffix.cr
@@ -4,7 +4,7 @@ require "./public_suffix/list"
 require "./public_suffix/rule"
 
 module PublicSuffix
-  VERSION = "1.1.20210324"
+  VERSION = "1.1.20210510"
 
   class DomainNotAllowed < Exception
   end
